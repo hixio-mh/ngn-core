4118dd876411655bb3c559a0dfda68671230ab47
Corey Butler <corey@coreybutler.com>
2017-04-14 12:33:00 -0500

    Added NGN.needs, custom error for missing dependencies, and moved the NGNX.TaskRunner feature to NGN.Tasks

--- core.js
+++ core.js
@@ -646,5 +646,60 @@ Object.defineProperties(NGN, {
         console.warn('%cDEPRECATION NOTICE: %c' + message, NGN.css, 'font-weight: normal;')
       }
     }, classFn)
+  }),
+
+  /**
+   * @method needs
+   * A method to check for the existance of required attributes in an object.
+   * This is designed to check for namespace existance.
+   *
+   * ```js
+   * NGN.uses(NGN, 'DOM','BUS', 'NET', 'JUNK') // Throws an error because "JUNK" doesn't exist.
+   * ```
+   * @param {Object} namespace
+   * The object to check.
+   * @param {String[]} attributes
+   * A list of attributes to check for.
+   * @private
+   */
+  needs: NGN.private(function (namespace, ...attributes) {
+    if (typeof namespace !== 'object') {
+      throw new Error('NGN.uses() requires an object.')
+    }
+
+    let missing = []
+
+    for (let value in attributes) {
+      if (!namespace.hasOwnProperty(value)) {
+        missing.push(value)
+      }
+    }
+
+    // Throw an error if there are any missing attributes.
+    if (missing.length > 0) {
+      throw new MissingDependencyError(`Missing ${namespace.constructor.name} dependencies: ${missing.join(', ')}`.replace(/\s{2,100}/gi, ' '))
+    }
+  }),
+
+  /**
+   * @method createAlias
+   * A helper method to alias a value on an object. This is the equivalent of:
+   * ```js
+   * Object.defineProperty(namespace, name, NGN.get(() => {
+   *   return value
+   * }))
+   * ```
+   * @param  {Object} namespace
+   * The object to apply the alias property to.
+   * @param  {String} name
+   * The alias name.
+   * @param  {Any} value
+   * The value to return.
+   * @private
+   */
+  createAlias: NGN.private(function (namespace, name, value) {
+    Object.defineProperty(namespace, name, NGN.get(() => {
+      return value
+    }))
   })
 })
